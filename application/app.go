2ae07d960738ddfc1998ed92142ce946f4ecfb98
Hassan Diab <hasan.diab.022001@gmail.com>
2025-02-23 16:48:43 +0200

    Graceful shutdown complete

--- application/app.go
+++ application/app.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -31,10 +32,33 @@ func (a *App) Start(ctx context.Context) error {
 	if err != nil {
 		return fmt.Errorf("faild to connect to redis: %w", err)
 	}
-	fmt.Println("hello working")
-	err = server.ListenAndServe()
-	if err != nil {
-		return fmt.Errorf("faild to start server: %w", err)
+
+	defer func() {
+		if err := a.rdb.Close(); err != nil {
+			fmt.Println("failed to close redis ", err)
+		}
+	}()
+
+	ch := make(chan error, 1)
+
+	go func() {
+
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("faild to start server: %w", err)
+		}
+		close(ch)
+	}()
+
+	// so we wont have a deadlock
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+		return server.Shutdown(timeout)
 	}
+
 	return nil
 }
