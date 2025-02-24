2f4030a7dd37aca296bf4639aab130883dc69469
Hassan Diab <hasan.diab.022001@gmail.com>
2025-02-24 20:21:31 +0200

    finish order api

--- application/app.go
+++ application/app.go
@@ -12,51 +12,58 @@ import (
 type App struct {
 	router http.Handler
 	rdb    *redis.Client
+	config Config
 }
 
-func New() *App {
+func New(config Config) *App {
 	app := &App{
-		router: loadRoutes(),
-		rdb:    redis.NewClient(&redis.Options{}),
+		rdb: redis.NewClient(&redis.Options{
+			Addr: config.RedisAddress,
+		}),
+		config: config,
 	}
+
+	app.loadRoutes()
 
 	return app
 }
 
 func (a *App) Start(ctx context.Context) error {
 	server := &http.Server{
-		Addr:    ":3000",
+		Addr:    fmt.Sprintf(":%d", a.config.ServerPort),
 		Handler: a.router,
 	}
+
 	err := a.rdb.Ping(ctx).Err()
 	if err != nil {
-		return fmt.Errorf("faild to connect to redis: %w", err)
+		return fmt.Errorf("failed to connect to redis: %w", err)
 	}
 
 	defer func() {
 		if err := a.rdb.Close(); err != nil {
-			fmt.Println("failed to close redis ", err)
+			fmt.Println("failed to close redis", err)
 		}
 	}()
+
+	fmt.Println("Starting server")
 
 	ch := make(chan error, 1)
 
 	go func() {
-
 		err = server.ListenAndServe()
 		if err != nil {
-			ch <- fmt.Errorf("faild to start server: %w", err)
+			ch <- fmt.Errorf("failed to start server: %w", err)
 		}
 		close(ch)
 	}()
 
-	// so we wont have a deadlock
 	select {
 	case err = <-ch:
 		return err
 	case <-ctx.Done():
 		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
 		defer cancel()
+
 		return server.Shutdown(timeout)
 	}
 
--- model/order.go
+++ model/order.go
@@ -8,7 +8,7 @@ import (
 
 type Order struct {
 	OrderID     uint64     `json:"order_id"`
-	CustomerId  uuid.UUID  `json:"customer_id"`
+	CustomerID  uuid.UUID  `json:"customer_id"`
 	LineItems   []LineItem `json:"line_items"`
 	CreatedAt   *time.Time `json:"created_at"`
 	ShippedAt   *time.Time `json:"shipped_at"`
@@ -16,7 +16,7 @@ type Order struct {
 }
 
 type LineItem struct {
-	ItemID   uuid.UUID
-	Quantity uint
-	Price    uint
+	ItemID   uuid.UUID `json:"item_id"`
+	Quantity uint      `json:"quantity"`
+	Price    uint      `json:"price"`
 }
